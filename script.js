3767b0325c11f736b307840005c4e5bc676cf94a
agil20 <100156146+agil20@users.noreply.github.com>
2022-05-22 15:25:26 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+ 
+  class Matchs{
+   
+    constructor(number)
+ {   
+    this.number=number;
+  
+ 
+ }
+plus(num){
+ this.number=this. number+num;
+ return this
+}
+ minus(num){
+ this.number=this. number-num;
+ return  this
+}
+multiple(num){
+  this.number=  this. number*num;
+  return this
+}
+divide(num){
+   this.number=this. number/num;
+   return this
+} }
+var result = new Matchs(50).plus(6).minus(30).multiple(3).divide(2)
+
+
+console.log(result.number);
+//2
+
+
+class Array {
+  constructor(arr){
+this.arr=arr
+
+  }
+  finds(value){
+let check=false
+for (let index = 0; index < this.arr.length; index++) {
+    const element = this.arr[index];
+    
+    if (value==element) {
+    check=true
+    }
+} 
+return check
+  }
+  push1(value){
+
+    this.arr[this.arr.length++]=value;
+     return this.arr
+}
+
+
+}
+
+
+let arr1=[1,2]
+let newarr=new Array(arr1).push1(8)
+let newarrs=new Array(arr1).finds(8)
+console.log(newarrs);
+console.log(newarr);
